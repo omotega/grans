429748a7185773f9e4c34e1b5d46331befe50a3d
omotega <tomoyibo@gmail.com>
2022-12-26 00:40:53 +0100

    feat:added application response function

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,6 +1,20 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 
-export const successResponse = (res:Response,statusCode:number,message:string,data:any = []) => {
-    const resObj = {res,statusCode,message,data}
-    return res.status(statusCode).send(resObj)
+export function successResponse(res: Response, statusCode: number, message: string, data: any = []) {
+  const resObj = {  statusCode, message, data }
+  return res.status(statusCode).send(resObj)
 }
+
+export function validateErrors(res: Response, statusCode: number, error: any) {
+  const resObj = { statusCode, error };
+  return res.status(statusCode).send(resObj);
+}
+
+export function handleError(req: Request, error: any) {
+  console.log(`
+        Error message: ${JSON.stringify(error.message)},
+        Caught At:${JSON.stringify(req.path)}.
+
+    `)
+}
+
