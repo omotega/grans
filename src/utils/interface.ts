ccaf79b5ac51566a36c75fa9379782b4db261718
omotega <tomoyibo@gmail.com>
2022-12-26 00:42:06 +0100

    feat:added interface

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -2,4 +2,32 @@ export interface Iuser {
   id?: string;
   name: string;
   password: string;
+  email: string;
+  active?: string;
+  photo?: string
+  createdAt?: Date;
+  updatedAt?: Date;
 }
+
+export interface CustomRequest {
+  user: Iuser
+  file: object,
+  params: object,
+  query: object,
+  path: object;
+}
+
+export interface Iotp {
+  id?: number;
+  email: string;
+  token: number;
+  expired: boolean;
+}
+
+export interface Iaccount {
+  id: number,
+  wallet_id: string,
+  user_id: number;
+  balance: number;
+}
+
