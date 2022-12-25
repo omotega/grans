0d180e26e7da88a9d820d615fcac409bbcf564ea
omotega <tomoyibo@gmail.com>
2022-12-26 00:46:41 +0100

    chore:project setup

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,32 @@
 import 'express-async-errors';
-import express from 'express';
+import express,{ Express,Request,Response } from 'express';
 import dotenv from 'dotenv'
-import { PrismaClient } from '@prisma/client'
-const prismaClient = new PrismaClient();
-
 
 import ErrorHandler from './middleware/errormiddleware';
+import { CustomRequest } from './utils/interface'
+import userRouter from './routes/user';
 
 dotenv.config();
 const port = process.env.PORT;
 
 const app = express();
 
+
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
+
+declare global {
+  namespace Express {
+    interface Request extends CustomRequest { }
+  }
+}
+
+app.get('/',(req:Request,res:Response) => {
+  res.send('welcome to this wallet application')
+})
 
 app.use(ErrorHandler);
+
+app.use('/api',userRouter);
 
 export default app
