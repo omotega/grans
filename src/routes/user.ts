e5e09a1dd64aca0dea90c7976d501c9ed481d779
omotega <tomoyibo@gmail.com>
2022-12-26 02:53:06 +0100

    feat: created verify account route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,9 +2,10 @@ import { Router } from 'express'
 
 const userRouter = Router();
 
-import { Register } from '../controllers/usercontroller'
+import { Register,verify } from '../controllers/usercontroller'
 import { validateRegisterMiddleware } from '../middleware/validate';
 
 userRouter.route('/signup').post(validateRegisterMiddleware,Register);
+userRouter.route('/verify').post(verify);
 
 export default userRouter;
