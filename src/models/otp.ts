2b25693b979380e28ec405a71d6e8e85eb3201dc
omotega <tomoyibo@gmail.com>
2023-01-01 01:56:47 +0100

    refactor: added fields the otp  model

--- src/models/otp.ts
+++ src/models/otp.ts
@@ -1,12 +1,16 @@
 import { DataTypes, Model } from 'sequelize';
 import db from './index'
+import { Iotp } from '../utils/interface';
 
-
-class Otp extends Model {
+class Otp extends Model<Iotp> {
   declare id: number;
   declare email: string;
-  declare token: number;
+  declare token: string;
   declare expired: boolean;
+
+  static associate(models:any) {
+    Otp.belongsTo(models.Users);
+  }
 
 }
 
@@ -23,11 +27,15 @@ Otp.init({
     unique: true,
   },
   token: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.STRING,
     allowNull: false,
   },
+  expiry_time:{
+    type:DataTypes.DATE,
+    allowNull:true,
+  },
   expired: {
-    type: DataTypes.STRING,
+    type: DataTypes.BOOLEAN,
     allowNull: false,
     defaultValue: false,
   }
