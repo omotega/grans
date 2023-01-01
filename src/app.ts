479de8e766595b33f1825c3f8168c3413d5edd58
omotega <tomoyibo@gmail.com>
2023-01-01 02:19:36 +0100

    chore: added incorrect route mideleware

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,39 @@
-import 'express-async-errors';
-import express,{ Express,Request,Response } from 'express';
-import dotenv from 'dotenv'
+import "express-async-errors";
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
-import ErrorHandler from './middleware/errormiddleware';
-import { CustomRequest } from './utils/interface'
-import userRouter from './routes/user';
+import ErrorHandler from "./middleware/errormiddleware";
+import { CustomRequest } from "./utils/interface";
+import userRouter from "./routes/user";
 
 dotenv.config();
 const port = process.env.PORT;
 
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 declare global {
   namespace Express {
-    interface Request extends CustomRequest { }
+    interface Request extends CustomRequest {}
   }
 }
 
-app.get('/',(req:Request,res:Response) => {
-  res.send('welcome to this wallet application')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("welcome to this wallet application");
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).send({
+    status: "error",
+    error: "Not found",
+    message: "Route not correct,kindly check url",
+  });
+});
 
 app.use(ErrorHandler);
 
-app.use('/api',userRouter);
+app.use("/api", userRouter);
 
-export default app
+export default app;
