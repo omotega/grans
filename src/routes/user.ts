dee9b3d0a37880310ce191885a17c912066709d0
omotega <tomoyibo@gmail.com>
2023-01-01 02:14:58 +0100

    feat:created login route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,10 +2,11 @@ import { Router } from 'express'
 
 const userRouter = Router();
 
-import { Register,verify } from '../controllers/usercontroller'
-import { validateRegisterMiddleware } from '../middleware/validate';
+import { Register,verify,login } from '../controllers/usercontroller'
+import { loginMiddleware, validateRegisterMiddleware } from '../middleware/validate';
 
 userRouter.route('/signup').post(validateRegisterMiddleware,Register);
 userRouter.route('/verify').post(verify);
+userRouter.route('/login').get(loginMiddleware,login);
 
 export default userRouter;
