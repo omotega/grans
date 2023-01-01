f107d3548fc73be706345976102ee6bd640e4ec4
omotega <tomoyibo@gmail.com>
2023-01-01 01:50:05 +0100

    feat: added interface for order,cart,delivery,restaurant and food

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -1,34 +1,77 @@
 export interface Iuser {
-  id?: string;
+  id?:number;
   name: string;
   password: string;
   email: string;
   active?: boolean;
-  verified?:boolean;
-  photo?: string
+  verified?: boolean;
+  photo?: string;
+  role?: string;
   createdAt?: Date;
   updatedAt?: Date;
 }
 
 export interface CustomRequest {
-  user: Iuser
-  file: object,
-  params: object,
-  query: object,
+  user: Iuser;
+  file: object;
+  params: object;
+  query: object;
   path: object;
 }
 
 export interface Iotp {
   id?: number;
   email: string;
-  token: number;
-  expired: boolean;
+  token: string;
+  expiry_time?: Date;
+  expired?: boolean;
 }
 
 export interface Iaccount {
-  id: number,
-  wallet_id: string,
-  user_id: number;
+  id?: number;
+  userId?: number;
   balance: number;
 }
 
+export interface Ifood {
+  id?: number;
+  price?: number;
+  foodType?: string;
+  description?: string;
+  vendor?: Iuser;
+}
+
+export interface Irestaurant {
+  id?:number;
+  name?: string;
+  state?: string;
+  city?: string;
+  address?: string;
+  description?: string;
+  isOpen?: boolean;
+}
+
+export interface Idelivery {
+  id?:number;
+  status: string;
+  message: string;
+}
+
+export interface Icart {
+  id?:number;
+  userId?: Iuser;
+  product?: [string];
+  bill?: number;
+}
+
+export interface Iorder {
+  id?:number;
+  owner?: Iuser;
+  item?: [any];
+  totalAmount?: number;
+  paidAmount?: number;
+  canceledAt?:Date;
+  orderDate?: Date;
+  orderStatus?: string;
+  remark?: string;
+}
