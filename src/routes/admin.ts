35862e903dfd6dc7e178098caee5e9f16ad2ea9e
omotega <tomoyibo@gmail.com>
2023-01-04 04:31:11 +0100

    feat:created updateuserrole  route

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 const adminRouter = Router();
 
-import { deactivateUser,activateDeactivateduser} from '../controllers/admin'
+import { deactivateUser,activateDeactivateduser, updateUserRole} from '../controllers/admin'
 import { verifyAdmin,guard } from '../middleware/auth'
 
 adminRouter.route('/users/activate/:userId').patch(guard,verifyAdmin,activateDeactivateduser);
 adminRouter.route('/users/deactivate/:userId').patch(guard,verifyAdmin,deactivateUser);
+adminRouter.route('/users/update/:userId').put(guard,verifyAdmin,updateUserRole);
 
 export default adminRouter;
