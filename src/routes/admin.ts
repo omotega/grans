f6799d4f6592c316fbeabca06e9d24e56bc6626b
omotega <tomoyibo@gmail.com>
2023-01-04 04:29:48 +0100

    feat:created the activatedeactivateduser  route

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express'
 const adminRouter = Router();
 
-import { deactivateUser} from '../controllers/admin'
+import { deactivateUser,activateDeactivateduser} from '../controllers/admin'
 import { verifyAdmin,guard } from '../middleware/auth'
 
-
+adminRouter.route('/users/activate/:userId').patch(guard,verifyAdmin,activateDeactivateduser);
 adminRouter.route('/users/deactivate/:userId').patch(guard,verifyAdmin,deactivateUser);
-
 
 export default adminRouter;
