ffcb8fce5dc4090c2bd5adec1694570987ac2d5c
omotega <tomoyibo@gmail.com>
2023-03-26 09:40:43 +0100

    refactore:refactored user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,14 +2,16 @@ import { Router } from 'express'
 
 const userRouter = Router();
 
-import { Register,verify,login,updateProfile } from '../controllers/usercontroller'
+import { Register,verify,login,updateProfile,logOut } from '../controllers/usercontroller'
 import { loginMiddleware, validateRegisterMiddleware } from '../middleware/validate';
-import { guard  } from '../middleware/auth'
+import { guard } from '../middleware/auth'
 
 userRouter.route('/users/signup').post(validateRegisterMiddleware,Register);
 userRouter.route('/users/verify').post(verify);
-userRouter.route('/users/login').get(loginMiddleware,login);
+userRouter.route('/users/login').get(login);
 
-userRouter.route('/users/profile').patch(guard,updateProfile);
+userRouter.route('/users/profile').patch(loginMiddleware,guard,updateProfile);
+userRouter.route('/users/logout').delete(logOut);
+
 
 export default userRouter;
