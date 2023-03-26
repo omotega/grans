eb6e5315a3ca440acf6b6f70bcd7aec51f9521b1
omotega <tomoyibo@gmail.com>
2023-03-26 09:37:51 +0100

    added packages globally

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
 import "express-async-errors";
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
+import httpLogger from "./logger/httplogger";
 import ErrorHandler from "./middleware/errormiddleware";
 import { CustomRequest } from "./utils/interface";
 import userRouter from "./routes/user";
 import adminRouter from "./routes/admin";
+import { guard }  from './middleware/auth'
 
 dotenv.config();
-const port = process.env.PORT;
+
 
 const app = express();
 
+app.use(httpLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 declare global {
   namespace Express {
@@ -25,10 +30,12 @@ app.get("/", (req: Request, res: Response) => {
   res.send("welcome to this wallet application");
 });
 
+
 app.use(ErrorHandler);
 
+
 app.use("/api", userRouter);
-app.use('/api',adminRouter);
+app.use("/api", adminRouter);
 
 app.use((req: Request, res: Response) => {
   res.status(404).send({
