66486736e562057aa1cfac526187364ce795a66a
omotega <tomoyibo@gmail.com>
2023-03-31 03:24:45 +0100

    refactor:updated error response

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import User from "../models/user";
 import db from "../models/index";
 import { NotFoundError, ServerError } from "../errors/apperrors";
-import { handleError, successResponse } from "../utils/response";
+import { errorResponse, handleError, successResponse } from "../utils/response";
 
 import Helper from "../utils/helper";
 
@@ -11,15 +11,15 @@ export const deactivateUser = async (req: Request, res: Response) => {
   try {
     const { userId } = req.params;
     const user = await User.findOne({ where: { id: userId }, transaction: t });
-    if (!user) throw new NotFoundError("user not found");
+    if (!user) return errorResponse(res,404,"user not found");
     await user.update({ active: false }, { transaction: t });
-    const details = Helper.excludeFields(["password"], user);
+    const details = Helper.excludeFields(["password"], user.dataValues);
     await t.commit();
     return successResponse(res, 200, "user deactivated", details);
   } catch (error) {
     await t.rollback();
     handleError(req, error);
-    throw new ServerError("something happened");
+    return errorResponse(res,500,"Something went wrong");
   }
 };
 export const activateDeactivateduser = async (req: Request, res: Response) => {
@@ -27,17 +27,18 @@ export const activateDeactivateduser = async (req: Request, res: Response) => {
   try {
     const { userId } = req.params;
     const user = await User.findOne({ where: { id: userId }, transaction: t });
-    if (!user) throw new NotFoundError("user not found");
+    if (!user) return errorResponse(res,404,"user not found");
     if (user.active != true) {
       await user.update({ active: true }, { transaction: t });
     }
-    const details = Helper.excludeFields(["password"], user);
+    const details = Helper.excludeFields(["password"], user.dataValues);
     await t.commit();
     return successResponse(res, 200, "user deactivated", details);
   } catch (error) {
     await t.rollback();
     handleError(req, error);
-    throw new ServerError("something happened");
+    return errorResponse(res,500,"Something went wrong");
+    
   }
 };
 
@@ -46,14 +47,14 @@ export const updateUserRole = async (req: Request, res: Response) => {
   try {
     const { userId } = req.params;
     const user = await User.findOne({ where: { id: userId }, transaction: t });
-    if (!user) throw new NotFoundError("user not found");
+    if (!user) return errorResponse(res,404,"user not found");
     await user.update({ role: "vendor" }, { transaction: t });
-    const details = Helper.excludeFields(["password"], user);
+    const details = Helper.excludeFields(["password"], user.dataValues);
     await t.commit();
     return successResponse(res, 200, "user deactivated", details);
   } catch (error) {
     await t.rollback();
     handleError(req, error);
-    throw new ServerError("something happened");
+    return errorResponse(res,500,"Something went wrong");
   }
 };
