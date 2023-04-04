1cb9166c7916a39029ebae5de3904ae3d053a299
omotega <tomoyibo@gmail.com>
2023-04-04 10:23:01 +0100

    created root file in routes

---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+import adminRouter  from './admin'
+import cardTransactionRouter from './cardTransaction'
+import userRouter from './user';
+
+const route = { adminRouter,cardTransactionRouter,userRouter };
+export default route;
