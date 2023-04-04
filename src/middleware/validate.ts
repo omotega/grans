ce737d71a17c03ec79b7833e8ad2580974c7a091
omotega <tomoyibo@gmail.com>
2023-04-04 10:13:01 +0100

    feat:added carddetails validation middleware

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,28 +1,58 @@
 import { Request, Response, NextFunction } from "express";
-import { validateErrors, handleError } from '../utils/response'
-import { loginValidation, registerValidation } from "../validations/uservalidation";
-import { ServerError } from "../errors/apperrors";
+import { validateErrors, handleError, errorResponse } from "../utils/response";
+import {
+  loginValidation,
+  registerValidation,
+} from "../validations/uservalidation";
+import { cardChargeValidation } from "../validations/cardValidation";
 
-export const validateRegisterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const validateRegisterMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const payload = req.body;
   try {
     const validate = registerValidation(payload);
-    if (validate.error) return validateErrors(res, 406, validate.error?.details[0].message);
+    if (validate.error)
+      return validateErrors(res, 406, validate.error?.details[0].message);
     next();
   } catch (error) {
-    handleError(req, error)
-    throw new ServerError('Something happened');
+    handleError(req, error);
+    return errorResponse(res, 500, "Something went wrong");
   }
-}
+};
 
-export const loginMiddleware = (req: Request, res:Response,next: NextFunction) => {
+export const loginMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const payload = req.body;
   try {
     const validate = loginValidation(payload);
-    if(validate.error) return validateErrors(res,406,validate.error?.details[0].message)
-    next()
+    if (validate.error)
+      return validateErrors(res, 406, validate.error?.details[0].message);
+    next();
   } catch (error) {
-    handleError(req, error)
-    throw new ServerError('Something happened');
+    handleError(req, error);
+    return errorResponse(res, 500, "Something went wrong");
   }
-}
+};
+
+export const cardValidationMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const payload = req.body;
+  try {
+    const validate = cardChargeValidation(payload);
+    if (validate.error)
+      return validateErrors(res, 406, validate.error?.details[0].message);
+    next();
+  } catch (error) {
+    handleError(req, error);
+    return errorResponse(res, 500, "Something went wrong");
+  }
+};
