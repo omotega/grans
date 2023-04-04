b197d544697aa4b61daeacf3882b64d7e0462366
omotega <tomoyibo@gmail.com>
2023-04-04 10:10:40 +0100

    refactor:updated server response

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import User from "../models/user";
-import db from "../models/index";
-import { NotFoundError, ServerError } from "../errors/apperrors";
+import db from "../models/index"
 import { errorResponse, handleError, successResponse } from "../utils/response";
 
 import Helper from "../utils/helper";
@@ -39,22 +38,5 @@ export const activateDeactivateduser = async (req: Request, res: Response) => {
     handleError(req, error);
     return errorResponse(res,500,"Something went wrong");
     
-  }
-};
-
-export const updateUserRole = async (req: Request, res: Response) => {
-  const t = await db.transaction();
-  try {
-    const { userId } = req.params;
-    const user = await User.findOne({ where: { id: userId }, transaction: t });
-    if (!user) return errorResponse(res,404,"user not found");
-    await user.update({ role: "vendor" }, { transaction: t });
-    const details = Helper.excludeFields(["password"], user.dataValues);
-    await t.commit();
-    return successResponse(res, 200, "user deactivated", details);
-  } catch (error) {
-    await t.rollback();
-    handleError(req, error);
-    return errorResponse(res,500,"Something went wrong");
   }
 };
