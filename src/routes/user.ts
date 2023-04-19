67cfb92f512111f7864a665bdd9bb8c2ea6d4fa8
omotega <tomoyibo@gmail.com>
2023-04-19 17:33:57 +0100

    refactor:refactored user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,12 +6,12 @@ import { Register,verify,login,updateProfile,logOut } from '../controllers/userc
 import { loginMiddleware, validateRegisterMiddleware } from '../middleware/validate';
 import { guard } from '../middleware/auth'
 
-userRouter.route('/users/signup').post(validateRegisterMiddleware,Register);
-userRouter.route('/users/verify').post(verify);
-userRouter.route('/users/login').get(loginMiddleware,login);
+userRouter.route('/signup').post(validateRegisterMiddleware,Register);
+userRouter.route('/verify').post(verify);
+userRouter.route('/login').get(loginMiddleware,login);
 
-userRouter.route('/users/profile').patch(guard,updateProfile);
-userRouter.route('/users/logout').delete(logOut);
+userRouter.route('/profile').patch(guard,updateProfile);
+userRouter.route('/logout').delete(logOut);
 
 
 export default userRouter;
