3f4511d9863ec0fe94113e5190d0b78ce529975c
omotega <tomoyibo@gmail.com>
2023-04-19 17:23:19 +0100

    feat:added sunmit pin amd submit otp controller function to the application

--- src/controllers/card_transasctions.ts
+++ src/controllers/card_transasctions.ts
@@ -5,16 +5,17 @@ import axios from "axios";
 import config from "../config/config";
 import CardTransaction from "../models/card_transaction";
 import db from "../models";
-import { charge } from "../helpers/paystack";
-import { creditAccount, debitAccount } from "../helpers/transactions";
+import { creditAccount } from "../helpers/transactions";
 import { errorResponse, handleError, successResponse } from "../utils/response";
+import Helper from "../utils/helper";
 
 const PAYSTACK_URL = "https://api.paystack.co/charge";
 
-
 export async function chargeCard(req: Request, res: Response) {
   let credit;
-  const { accountId, pan, expiry_month, expiry_year, cvv, email, amount } = req.body;
+  const { accountId, pan, expiry_month, expiry_year, cvv, email, amount } =
+    req.body;
+
   const t = await db.transaction();
   try {
     const cardCharge = await axios.post(
@@ -23,8 +24,8 @@ export async function chargeCard(req: Request, res: Response) {
         card: {
           number: pan,
           cvv: cvv,
-          expiry_year: expiry_year,
           expiry_month: expiry_month,
+          expiry_year: expiry_year,
         },
         amount: amount,
         email: email,
@@ -32,16 +33,9 @@ export async function chargeCard(req: Request, res: Response) {
       {
         headers: {
           Authorization: `Bearer ${config.PAYSTACK_SECRET_KEY}`,
-          "Content-Type": "application/json",
         },
       }
     );
-    const card_transaction = await CardTransaction.create({
-      externalReference: cardCharge.data.data.reference,
-      accountId: accountId,
-      amount: amount,
-      lastResponse: cardCharge.data.data.status,
-    });
     if (cardCharge.data.data.status === "success") {
       credit = await creditAccount(
         {
@@ -56,11 +50,181 @@ export async function chargeCard(req: Request, res: Response) {
         await t.rollback();
         return { success: false, message: "credit failed" };
       }
+      const card_transaction = await CardTransaction.create({
+        externalReference: cardCharge.data.data.reference,
+        accountId: accountId,
+        amount: amount,
+        lastResponse: cardCharge.data.data.status,
+      });
+      const reference = cardCharge.data.data.reference;
+      req.reference = reference;
+      await t.commit();
+      return successResponse(res, 200, "charge successful", reference);
+    } else {
+      const card_transaction = await CardTransaction.create({
+        externalReference: cardCharge.data.data.reference,
+        accountId: accountId,
+        amount: amount,
+        lastResponse: cardCharge.data.data.status,
+      });
+      const transactionreference = cardCharge.data.data.reference;
+      
+      const reference = await Helper.generateToken(
+        {
+          reference: transactionreference,
+        },
+        config.ACCESS_TOKEN_SECRET
+      );
+      res.cookie("reference", reference, {
+        maxAge: 300000,
+        httpOnly: true,
+        path: "/",
+      });
+      await t.commit();
+      return successResponse(
+        res,
+        200,
+        "please submit pin",
+        transactionreference
+      );
     }
-    await t.commit();
-    return successResponse(res,200,'charge successful')
   } catch (error: any) {
-    handleError(req,error);
+    handleError(req, error);
+    await t.rollback();
+    return errorResponse(res, 500, "Something went wrong");
+  }
+}
+
+export const submitPin = async (req: Request, res: Response) => {
+  const t = await db.transaction();
+  try {
+    const { pin } = req.body;
+    const reference = req.reference;
+    const url = `${PAYSTACK_URL}/submit_pin`;
+    console.log(url, "URL OOO");
+    console.log(pin, reference);
+    const transaction = await CardTransaction.findOne({
+      where: { externalReference: reference },
+    });
+
+    console.log(transaction?.dataValues, "THIS IS TRANSACTION");
+    if (!transaction) {
+      return { message: "transaction not found" };
+    }
+    if (transaction.dataValues.lastResponse === "success") {
+      return { message: "transaction already complete" };
+    }
+    const charge = await axios.post(
+      url,
+      {
+        reference: reference,
+        pin: pin,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${config.PAYSTACK_SECRET_KEY}`,
+        },
+      }
+    );
+    console.log(charge.data, "THIS IS CHARGE INFORMATION");
+
+    if (charge.data.data.status === "success") {
+      console.log("YOU SURE SAY THE REQUEST REACH HERE");
+      const credit = await creditAccount(
+        {
+          amount: charge.data.data.amount,
+          accountId: transaction.dataValues.accountId,
+          purpose: "card-funding",
+          metadata: {
+            externalReference: reference,
+          },
+        },
+        t
+      );
+      // @ts-ignore
+      if (credit.success !== true) {
+        await t.rollback();
+        return { success: false, message: "credit failed" };
+      }
+      const card_transaction = await CardTransaction.update(
+      { lastResponse: charge.data.data.status },
+      { where: { externalReference: reference } }
+    );
+    await t.commit();
+    return successResponse(res, 200, "credit successful", reference);
+    } else {
+      if(charge.data.data.status === 'send_otp') {
+         const card_transaction = await CardTransaction.update(
+      { lastResponse: charge.data.data.status },
+      { where: { externalReference: reference } }
+    );
+      }
+       await t.commit();
+    return successResponse(res, 200, "credit please submit otp", reference);
+    }
+  } catch (error) {
+    handleError(req, error);
+    await t.rollback();
+    return errorResponse(res, 500, "Something went wrong");
+  }
+};
+
+export const submitOtp = async (req:Request,res:Response) => {
+  const t = await db.transaction();
+  try {
+    const { otp } = req.body;
+    const reference = req.reference;
+    const url = `${PAYSTACK_URL}/submit_otp`;
+
+    const transaction = await CardTransaction.findOne({
+      where: { externalReference:reference },
+    });
+    if (!transaction) {
+      return { message: "transaction not found" };
+    }
+    if (transaction.dataValues.lastResponse === "success") {
+      return { message: "transaction already complete" };
+    }
+    const charge = await axios.post(
+      url,
+      {
+        reference: reference,
+        otp: otp,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${config.PAYSTACK_SECRET_KEY}`,
+        },
+      }
+    );
+    if (charge.data.data.status === "success") {
+      console.log("YOU SURE SAY THE REQUEST REACH HERE");
+      await CardTransaction.update(
+        { lastResponse: charge.data.data.status },
+        { where: { externalReference: reference} }
+      );
+      const credit = await creditAccount(
+        {
+          amount: charge.data.data.amount,
+          accountId: transaction.dataValues.accountId,
+          purpose: "card-funding",
+          metadata: {
+            externalReference: reference,
+          },
+        },
+        t
+      );
+      // @ts-ignore
+      if (credit.success !== true) {
+        await t.rollback();
+        return { success: false, message: "credit failed" };
+      }
+       await t.commit();
+    return successResponse(res, 200, "credit successful", reference);
+    }
+    
+  } catch (error) {
+    handleError(req, error);
     await t.rollback();
     return errorResponse(res, 500, "Something went wrong");
   }
