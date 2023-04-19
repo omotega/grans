d5b07149d7d77769c879c93877a93c89a1aefe35
omotega <tomoyibo@gmail.com>
2023-04-19 17:32:40 +0100

    refactored database config

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,12 @@
-import { Model,Sequelize } from 'sequelize'
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-import * as config from '../config/database';
+import * as config from '../config/database'
 
 const db = new Sequelize(config);
 
-export default db
+export default db;
+
+
+
