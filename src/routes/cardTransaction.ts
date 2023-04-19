4cc2825213c1f91a911a18a97c442a5caa0be5f2
omotega <tomoyibo@gmail.com>
2023-04-19 17:24:32 +0100

    feat:added submit pin and sumit otp routes

--- src/routes/cardTransaction.ts
+++ src/routes/cardTransaction.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 
 const cardTransactionRouter = Router();
 
-import { chargeCard } from "../controllers/card_transasctions";
+import { chargeCard,submitPin,submitOtp } from "../controllers/card_transasctions";
 import { guard } from "../middleware/auth";
 import { cardValidationMiddleware } from "../middleware/validate";
+import transactionRef from "../middleware/paystack";
 
 cardTransactionRouter
   .route("/fundwallet")
-  .post(guard, chargeCard);
+  .post(guard,chargeCard);
+cardTransactionRouter.route('/submitpin').post(transactionRef,submitPin)
+cardTransactionRouter.route('/submitotp').post(transactionRef,submitOtp);
 
 export default cardTransactionRouter;
