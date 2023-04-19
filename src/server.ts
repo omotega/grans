d46d1a6ceded798e9158be5f89d263fb8c6d6735
omotega <tomoyibo@gmail.com>
2023-04-19 17:36:59 +0100

    refactored server

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,8 @@ import { Dbconnection } from './models/dbconnection'
 
 const port = process.env.PORT || 6666;
 
- app.listen(port, async () => {
-  console.log(`server connected on port ${port}`);
+const server = app.listen(port, async () => {
+  logger.info(`server connected on port ${port}`);
   try {
     await Dbconnection.authenticate();
     logger.info('database connected')
@@ -19,7 +19,10 @@ const port = process.env.PORT || 6666;
   }
 })
 
+
 process.on('unhandledRejection', (error: any) => {
   logger.info(`unhandledRejection: ${error.message}`)
   process.exit(1);
 })
+
+export default server;
