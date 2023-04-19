24494452120761a88bc3778be60d2c61868f5d5c
omotega <tomoyibo@gmail.com>
2023-04-19 17:35:00 +0100

    chore:added routes to a root file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import adminRouter  from './admin'
 import cardTransactionRouter from './cardTransaction'
 import userRouter from './user';
+import transactionRouter from './transaction';
 
-const route = { adminRouter,cardTransactionRouter,userRouter };
+const route = { adminRouter,cardTransactionRouter,userRouter,transactionRouter };
 export default route;
