d3e42e3d8ffef812c10b7cc02ab2dd45731645d6
omotega <tomoyibo@gmail.com>
2023-04-24 08:40:40 +0100

    feat:created transfer route

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 
 const transactionRouter = Router();
-import { deposit } from '../controllers/transactions'
+import { deposit,transfer } from '../controllers/transactions'
 
 transactionRouter.route('/deposit').post(deposit)
+transactionRouter.route('/transfer').post(transfer)
 
 export default transactionRouter
