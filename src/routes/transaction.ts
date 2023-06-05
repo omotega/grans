6b81096a85e412b965e34744789f29ae20690751
omotega <tomoyibo@gmail.com>
2023-06-05 18:02:26 +0100

    chore:added guard middleware to transfer route

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -2,8 +2,9 @@ import { Router } from 'express'
 
 const transactionRouter = Router();
 import { deposit,transfer } from '../controllers/transactions'
+import { guard } from '../middleware/auth';
 
 transactionRouter.route('/deposit').post(deposit)
-transactionRouter.route('/transfer').post(transfer)
+transactionRouter.route('/transfer').post(guard,transfer)
 
 export default transactionRouter
