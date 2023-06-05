3d709948fe006f3965a9b84478e59c6ae5c590d4
omotega <tomoyibo@gmail.com>
2023-06-05 18:21:34 +0100

    feat:added withdrwal roure

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 
 const transactionRouter = Router();
-import { deposit,transfer } from '../controllers/transactions'
+import { deposit,transfer,withdrawl } from '../controllers/transactions'
 import { guard } from '../middleware/auth';
 
 transactionRouter.route('/deposit').post(deposit)
 transactionRouter.route('/transfer').post(guard,transfer)
+transactionRouter.route('/withdrawl').post(guard,withdrawl)
 
 export default transactionRouter
