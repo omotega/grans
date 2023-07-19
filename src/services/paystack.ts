6f844bbfbdbae0575859ee0ea916f98e8f47d2d3
omotega <tomoyibo@gmail.com>
2023-07-19 02:07:24 +0100

    added paystack services function

---
+++ src/services/paystack.ts
@@ -0,0 +1,78 @@
+import fetch from "node-fetch";
+import config from "../config/config";
+
+const PAYSTACK_BASE_URL = "https://api.paystack.co/charge";
+
+const chargeCard = async (payload: any) => {
+  const input = {
+    card: {
+      number: payload.number,
+      cvv: payload.cvv,
+      expiry_month: payload.expiry_month,
+      expiry_year: payload.expiry_year,
+    },
+    amount: payload.amount,
+    email: payload.email,
+  };
+  const response = await fetch(PAYSTACK_BASE_URL, {
+    method: "post",
+    body: JSON.stringify(input),
+    headers: {
+      authorization: `Bearer ${config.PAYSTACK_SECRET_KEY}`,
+    },
+  });
+  const data = await response.json();
+  return data;
+};
+
+const submitPin = async (payload: any) => {
+  const url = `${PAYSTACK_BASE_URL}/submit_pin`;
+  const input = {
+    reference: payload.reference,
+    pin: payload.pin,
+  };
+  const response = await fetch(url, {
+    method: "post",
+    body: JSON.stringify(input),
+    headers: {
+      authorization: `Bearer ${config.PAYSTACK_SECRET_KEY}`,
+    },
+  });
+  const data = await response.json();
+  return data;
+};
+
+const submitPhone = async (payload: any) => {
+  const url = `${PAYSTACK_BASE_URL}/submit_otp`;
+  const input = {
+    reference: payload.reference,
+    otp: payload.otp,
+  };
+  const response = await fetch(url, {
+    method: "post",
+    body: JSON.stringify(input),
+    headers: {
+      authorization: `Bearer ${config.PAYSTACK_SECRET_KEY}`,
+    },
+  });
+  const data = await response.json();
+  console.log(data,'what is the data')
+  return data;
+  
+};
+// chargeCard({
+//   number: "507850785078507812",
+//   cvv: "081",
+//   expiry_month: "07",
+//   expiry_year: "24",
+//   amount: 10000,
+//   email: "tomoyibo@gmail.com",
+// })
+//   .then(console.log)
+//   .catch(console.log);
+
+export default {
+  chargeCard,
+  submitPin,
+  submitPhone,
+};
