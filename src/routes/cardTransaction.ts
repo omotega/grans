5c744c99e155c9f6d8dc957fed7ba84b59783be7
omotega <tomoyibo@gmail.com>
2023-07-19 02:44:47 +0100

    added cardtransaction route

--- src/routes/cardTransaction.ts
+++ src/routes/cardTransaction.ts
@@ -2,15 +2,19 @@ import { Router } from "express";
 
 const cardTransactionRouter = Router();
 
-import { chargeCard,submitPin,submitOtp } from "../controllers/card_transasctions";
+import cardtransasctionscontroller from "../controllers/card_transasctions";
 import { guard } from "../middleware/auth";
 import { cardValidationMiddleware } from "../middleware/validate";
 import transactionRef from "../middleware/paystack";
 
 cardTransactionRouter
   .route("/fundwallet")
-  .post(guard,chargeCard);
-cardTransactionRouter.route('/submitpin').post(transactionRef,submitPin)
-cardTransactionRouter.route('/submitotp').post(transactionRef,submitOtp);
+  .post(guard, cardtransasctionscontroller.chargeCard);
+cardTransactionRouter
+  .route("/submitpin")
+  .post(transactionRef, cardtransasctionscontroller.submitPin);
+cardTransactionRouter
+  .route("/submitotp")
+  .post(transactionRef, cardtransasctionscontroller.submitPin);
 
 export default cardTransactionRouter;
