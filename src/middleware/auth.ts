2d9d6999ae042803949bc8c944aa333602588d27
omotega <tomoyibo@gmail.com>
2023-08-14 16:06:15 +0100

    refactored auth middleware functions

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,50 +1,29 @@
 import { Request, Response, NextFunction } from "express";
-import User from "../models/user";
 import Helper from "../utils/helper";
-import { errorResponse, handleError } from "../utils/response";
 import config from "../config/config";
-import Session from "../models/session";
-import { reIssueAccessToken } from "../services/sessionservices";
+import { USER_NOT_FOUND } from "../utils/constant";
+import userrepo from "../database/repo/userrepo";
+import httpStatus from "http-status";
 
 export const guard = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const accessToken = req.cookies.accessToken;
-  const refreshToken = req.cookies.refreshToken;
-  try {
-    let newAccessToken;
-    let payload;
-    const result: any = Helper.decodeToken(
-      refreshToken,
-      config.REFRESH_TOKEN_SECRET
+  if (req.headers && req.headers.authorization) {
+    const token = req.headers.authorization.split(" ")[1];
+    const decode: any = await Helper.decodeToken(
+      token,
+      config.ACCESS_TOKEN_SECRET
     );
-    if (!refreshToken ) {
-      return errorResponse(res, 400, "please login");
-    } else if (accessToken === undefined && result.expired === false) {
-      newAccessToken = await reIssueAccessToken(refreshToken);
-      if (!newAccessToken) return next();
-      req.cookies.accessToken = newAccessToken;
-      const newToken = Helper.decodeToken(
-        newAccessToken,
-        config.ACCESS_TOKEN_SECRET
-      );
-      // @ts-ignore
-      req.User = newToken.payload;
-      return next();
-    } else {
-      payload = Helper.decodeToken(accessToken, config.ACCESS_TOKEN_SECRET);
-      if (payload) {
-        // @ts-ignore
-        req.User = payload.payload;
-        return next();
-      }
-    }
+    const user = await userrepo.findUserById(decode.id);
+    if (!user) throw new Error(USER_NOT_FOUND);
+    req.User = user;
     return next();
-  } catch (error) {
-    handleError(req, error);
-    return errorResponse(res, 500, "Something went wrong");
+  } else {
+    res
+      .status(httpStatus.BAD_REQUEST)
+      .json({ message: "authorization not found" });
   }
 };
 
@@ -53,14 +32,12 @@ export const verifyAdmin = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const { id } = req.User;
-    const user = await User.findOne({ where: { id, role: "admin" } });
-    if (!user)
-      return errorResponse(res, 401, "user not an admin,not authorized");
-    return next();
-  } catch (error) {
-    handleError(req, error);
-    return errorResponse(res, 500, "Something went wrong");
-  }
+  const { id } = req.User;
+  const user = await userrepo.findUserByRole({ userId: id, role: "ADMIN" });
+  if (!user) throw new Error(USER_NOT_FOUND);
+  next();
+};
+
+export default {
+  guard,
 };
