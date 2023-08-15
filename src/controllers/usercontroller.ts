81ac0ea5efa937967846197abdc766d410d74dc6
omotega <tomoyibo@gmail.com>
2023-08-15 18:13:19 +0100

    feat:added updateprofile controller function

--- src/controllers/usercontroller.ts
+++ src/controllers/usercontroller.ts
@@ -2,7 +2,11 @@ import { Request, Response } from "express";
 import Helper from "../utils/helper";
 import userservices from "../services/userservices";
 import httpStatus from "http-status";
-import { LOGIN_SUCCESSFUL, SIGNUP_SUCCESSFUL } from "../utils/constant";
+import {
+  LOGIN_SUCCESSFUL,
+  SIGNUP_SUCCESSFUL,
+  USER_PROFILE_UPDATE_SUCCESFUL,
+} from "../utils/constant";
 
 const Register = async (req: Request, res: Response) => {
   const { name, email, password, profilePicture, phoneNumber } = req.body;
@@ -31,7 +35,23 @@ const login = async (req: Request, res: Response) => {
   });
 };
 
+const updateProfile = async (req: Request, res: Response) => {
+  const { id } = req.User;
+  const { email, password } = req.body;
+  const hash = await Helper.hashPassword(password);
+  const response = await userservices.updateProfile({
+    userId: id,
+    email: email,
+    password: hash,
+  });
+  res.status(httpStatus.OK).json({
+    message: USER_PROFILE_UPDATE_SUCCESFUL,
+    user: Helper.excludeFields(["password"], response),
+  });
+};
+
 export default {
   Register,
   login,
+  updateProfile,
 };
