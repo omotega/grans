1b3eaaacfaebbb3bd261210822e84e2e3a0a3f82
omotega <tomoyibo@gmail.com>
2023-08-16 15:41:50 +0100

    feat:added card transaction

---
+++ src/controllers/fundwallet/card_transasctions.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import cardtranscationservice from "../../services/fundwallet/cardtranscationservice";
+import httpStatus from "http-status";
+
+async function chargeCard(req: Request, res: Response) {
+  const { number, expiry_month, expiry_year, cvv, email, amount, accountId } =
+    req.body;
+  const response = await cardtranscationservice.fundWalletWithCard({
+    number,
+    expiry_month,
+    expiry_year,
+    cvv,
+    email,
+    amount,
+    accountId: accountId,
+  });
+  res.status(httpStatus.OK).json(response);
+}
+
+const submitPin = async (req: Request, res: Response) => {
+  const { pin, accountId, reference } = req.body;
+  const response = await cardtranscationservice.submitPin({
+    pin,
+    accountId,
+    reference,
+  });
+  res.status(httpStatus.OK).json(response);
+};
+
+const submitOtp = async (req: Request, res: Response) => {
+  const { accountId, otp, reference } = req.body;
+  const response = await cardtranscationservice.submitPhone({
+    accountId,
+    otp,
+    reference,
+  });
+  res.status(httpStatus.OK).json(response);
+};
+
+export default {
+  chargeCard,
+  submitPin,
+  submitOtp,
+};
