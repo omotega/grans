37f6a4fff06a700d518fab7a72b760b45701fbd6
omotega <tomoyibo@gmail.com>
2023-08-29 18:16:34 +0100

    bank transfer route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
-// import adminRouter  from './admin'
-// import cardTransactionRouter from './cardTransaction'
 import { Router } from "express";
 import userRouter from "./user";
 import transactionRouter from "./transaction";
+import cardTransactionRouter from "./fundwallet/cardTransaction";
+import bankTransferRouter from "./fundwallet/banktransfer";
 
 const route = Router();
 
 route.use("/user", userRouter);
 route.use("/transaction", transactionRouter);
+route.use("/cardtransaction", cardTransactionRouter);
+route.use("/funwallet", bankTransferRouter);
 
 export default route;
