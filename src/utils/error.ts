268bef792cedea07d5bfe94443008739391fffb3
omotega <tomoyibo@gmail.com>
2023-10-05 19:14:44 +0100

    added error handling to application

---
+++ src/utils/error.ts
@@ -0,0 +1,27 @@
+interface AppErrorArgs {
+  name?: string;
+  httpCode: number;
+  description: string;
+  isOperational?: boolean;
+}
+
+export class AppError extends Error {
+  public readonly name: string;
+  public readonly httpCode: number;
+  public readonly isOperational: boolean = true;
+
+  constructor(args: AppErrorArgs) {
+    super(args.description);
+
+    Object.setPrototypeOf(this, new.target.prototype);
+
+    this.name = args.name || "Error";
+    this.httpCode = args.httpCode;
+
+    if (args.isOperational !== undefined) {
+      this.isOperational = args.isOperational;
+    }
+
+    Error.captureStackTrace(this);
+  }
+}
